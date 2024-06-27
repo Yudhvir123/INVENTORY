fa8a92358168c3995587039133ae995f2765becd
Yudhvir123 <y0261522@gmail.com>
2024-06-27 15:48:27 +0530

    Add files via upload

---
+++ app/Nova/Filters/UidFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Carbon\Carbon;
+use Laravel\Nova\Filters\Filter;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class UidFilter extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(NovaRequest $request, $query, $value)
+    {
+        $value=Carbon::parse($value);
+        return $query->whereHas('UID',fn($query)=> $query->where('UID','>=',$value));
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function options(NovaRequest $request)
+    {
+        return [];
+    }
+}
